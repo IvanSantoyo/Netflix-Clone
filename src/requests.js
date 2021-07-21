c75ff74d3725fa0303e6261062a3b3262e4e6b8c
Ivan Santoyo <ivan@Ivans-MacBook-Pro.local>
2021-07-21 14:45:35 -0700

    first commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "8b499063f7ac0a66ddb718c7923756e5"
+
+
+const requests = {
+    fetchTranding: `/trending/all/wek?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key${API_KEY}&language=en-US`,
+    feathcActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fethcHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocuments: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+export default requests;
