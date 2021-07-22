0c234dc2658f4e331b0aa0453186175b11fe6a76
Ivan Santoyo <ivan@Ivans-MacBook-Pro.local>
2021-07-21 23:34:52 -0700

    useEffect

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,23 @@
-import React from "react"
-function Row(){
-    return (
-        <div>
+import React, {useState, useEffect} from 'react'
+import axios from './axios';
 
+function Row({title, fetchUrl}){
+const [movies,setMovies] = useState([]);
 
-        </div>
-    )
+useEffect(() =>{
+  async function fetchData(){
+    const request = await axios.get(fetchUrl);
+    console.log(request);
+    return request;
+  }
+  fetchData();
+},[]);
+return (
+    <div>
+        <h2>{title}</h2>
+
+        {/* container */}
+    </div>
+)
 }
-export default Row;
+export default Row
--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const API_KEY = "8b499063f7ac0a66ddb718c7923756e5"
 
 
 const requests = {
-    fetchTranding: `/trending/all/wek?api_key=${API_KEY}&language=en-US`,
+    fetchTrending: `/trending/all/wek?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key${API_KEY}&language=en-US`,
     feathcActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
